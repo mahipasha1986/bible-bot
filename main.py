e8091f6a10870d4f9de0566792b8ea0ce7643b1e
mahipasha1986 <mahi.pasha1986@gmail.com>
2026-06-17 15:58:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -979,7 +979,7 @@ async function searchBibleVerse(){
                     color:#1f4e79;
                     margin-bottom:10px;
                     ">
-                        ${v.chapter_number}:${v.verse_number}
+                        ${v.book_name || v.name_fa || v.book || ""} ${v.chapter_number}:${v.verse_number}
                     </div>
 
                     <div style="
