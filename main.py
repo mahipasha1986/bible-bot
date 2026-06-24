3dd9a7b0a29c9ae82d8def132213bbb4337d6c6e
mahipasha1986 <mahi.pasha1986@gmail.com>
2026-06-24 15:50:52 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2846,10 +2846,11 @@ function renderPagination(container){
   nav.style.gap = "10px";
 
   const prev = document.createElement("button");
-  prev.innerHTML = `صفحه قبل
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" style="vertical-align:middle;margin-right:8px">
+  prev.innerHTML = `
+  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" style="vertical-align:middle;margin-left:8px">
   <path d="M15 6L9 12L15 18" stroke="#111827" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-  </svg>`;
+  </svg>
+  صفحه قبل`;
   prev.className = "blue";
   prev.style.flex = "1";
   prev.disabled = currentPage === 0;
@@ -2865,10 +2866,10 @@ function renderPagination(container){
 
   const next = document.createElement("button");
   next.innerHTML = `
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" style="vertical-align:middle;margin-left:8px">
+  صفحه بعد
+  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" style="vertical-align:middle;margin-right:8px">
   <path d="M9 6L15 12L9 18" stroke="#111827" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-  </svg>
-  صفحه بعد`;
+  </svg>`;
   next.style.width = "180px";
   next.style.height = "50px";
   next.style.background = "#ffffff";
